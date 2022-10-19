b0e6ffe82bb8c4b19bcff407b50ee36b1dd51d0a
B20037-Kobayashi <e1b20037@oit.ac.jp>
2022-10-19 09:57:23 +0900

    初期化

---
+++ src/main/java/personallec04/plec04/Plec04Application.java
@@ -0,0 +1,13 @@
+package personallec04.plec04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plec04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plec04Application.class, args);
+	}
+
+}
---
+++ src/test/java/personallec04/plec04/Plec04ApplicationTests.java
@@ -0,0 +1,13 @@
+package personallec04.plec04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Plec04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
